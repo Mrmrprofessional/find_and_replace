6094770d4bfb688b6b3b705326fbad019ab83153
mike <michael.rapp358@gmail.com>
2015-09-11 09:20:09 -0700

    add first spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,5 @@
-describe('romanNumeral', function() {
-    it("displays I for the value of 1", function(){
-    expect(romanNumeral(1)).to.equal("I");
-    });
-
-    it("displays LXVI for the value of 66", function(){
-        expect(romanNumeral(999)).to.equal("CMXCIX");
+describe('findReplace', function(){
+    it("displays the input as the output", function(){
+        expect(findReplace("Hello world", "world", "universe")).to.equal("Hello world");
     });
 });
